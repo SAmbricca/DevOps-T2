3a6d1443c476067e22951cf5282874c9af6aa70e
Santiago Ambricca <117587525+SAmbricca@users.noreply.github.com>
2024-04-03 00:38:51 -0300

    Create main.tf

---
+++ Ms-proveedores-dev/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+region = "us-east-1" 
+}
+
+#Crear un nuevo grupo de seguridad
+
+resource "aws_security_group" "instance_security_group" {
+name        = "instance_security_group"
+description = "Security group for EC2 instance"
+}
+
+resource "aws_instance" "PROVEEDORES_dev_instance" {
+ami           = "ami-0a3c3a20c09d6f377"  
+instance_type = "t2.micro"               
+
+#Asociar la instancia con el grupo de seguridad recién creado
+
+vpc_security_group_ids = [aws_security_group.instance_security_group.id]
+
+#Tag para identificar la instancia
+
+tags = {
+Name = "ORG-PROVEEDORES-DEV"
+}
+}
+
+#Salida para mostrar la IP pública de la instancia EC2 después del despliegue
+
+output "public_ip_dev" {
+value = aws_instance. PROVEEDORES_dev_instance.public_ip
+}
